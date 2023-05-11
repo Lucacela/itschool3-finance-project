5aa2e65d084d1989163670e4af62a9d9173296a4
Lucacela <m.lucacela@yahoo.com>
2023-05-11 09:45:55 +0300

    Update v0.6

--- finance-project/main.py
+++ finance-project/main.py
@@ -1,9 +1,9 @@
 import logging
-from fastapi import FastAPI, Request
-from fastapi_utils.tasks import repeat_every
 import subprocess
 import os
 import time
+from fastapi import FastAPI, Request
+from fastapi_utils.tasks import repeat_every
 from api.users import users_router
 from api.assets import assets_router
 from domain.user.factory import InvalidUsername
@@ -17,11 +17,10 @@ logging.basicConfig(
 
 app = FastAPI(
     debug=True,
-    title="Fintech Portfolio API",
-    # TODO add to README
+    title="Fintech Portofolio API",
     description="A webserver with a REST API for keeping track of your different financial assets,"
     " stocks & crypto, and see/compare their evolution",
-    version="0.6.0",
+    version="0.3.0",
 )
 app.include_router(users_router)
 app.include_router(assets_router)
@@ -30,7 +29,7 @@ app.include_router(assets_router)
 @app.exception_handler(InvalidUsername)
 def return_invalid_username(_: Request, e: InvalidUsername):
     return JSONResponse(
-        status_code=400, content="Username is not valid! Error: " + str(e)
+        status_code=400, content="Username is not valid. Error: " + str(e)
     )
 
 
@@ -42,16 +41,19 @@ def clean_images_older_than_24h():
     current_posix_time = time.time()
     ago_24h = current_posix_time - 60 * 60 * 24
     for g in graphs:
+        print(g)
         g_creation_time = os.path.getctime(g)
         if g_creation_time < ago_24h:
             os.remove(g)
 
 
 if __name__ == "__main__":
-    logging.info("Starting webserver ...")
+    import subprocess
+
+    logging.info("Starting webserver...")
     try:
-        subprocess.run(["uvicorn", "main:app", "--reload"])
+        subprocess.run(["uvicorn", "finance-project.main:app", "--reload"])
     except KeyboardInterrupt as e:
-        logging.warning("Keyboard interrupt.")
+        logging.warning("KeyBoard interrupt.")
     except Exception as e:
         logging.warning("Webserver has stopped. Reason: " + str(e))
